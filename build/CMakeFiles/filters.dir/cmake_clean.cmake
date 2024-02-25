fdd5983b9e90e5dc63192e8e85b5c314997f51f6
Vansh Chhabra <86122440+v2pir@users.noreply.github.com>
2024-02-24 21:36:09 -0800

    Add files via upload

---
+++ build/CMakeFiles/filters.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filters.dir/filters.cpp.o"
+  "filters.cpython-38-x86_64-linux-gnu.so"
+  "filters.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
